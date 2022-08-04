cbd720537bb108709f2a4590bcb6d464216792ab
Alejandro HM <alhemoasde@gmail.com>
2022-08-03 22:38:14 -0500

    #11 alhemoasde: Export Excel Suscriptores

--- resources/views/subscribers/index.blade.php
+++ resources/views/subscribers/index.blade.php
@@ -1,8 +1,10 @@
 @extends('index')
 
 @section('css')
-    <link href="{{ asset('/assets/vendor/data-tables/DataTables-1.11.5/css/dataTables.bootstrap5.min.css') }}"
-        rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.bootstrap5.min.css" />
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.datatables.net/responsive/2.3.0/css/responsive.bootstrap5.min.css" />
 @endsection
 
 @section('content')
@@ -46,20 +48,24 @@
                                 <td>{{ date('d/m/Y', strtotime($subscriber->created_at)) }}</td>
                                 <td>{{ $subscriber->name }}</td>
                                 <td>{{ $subscriber->email }}</td>
-                                <td>{{ $subscriber->status == 1 ? 'Activo' : 'Inactivo'}}</td>
+                                <td>{{ $subscriber->status == 1 ? 'Activo' : 'Inactivo' }}</td>
 
                                 <td class="text-center">
                                     <div class="btn-group">
-                                        <a class="btn btn-outline-secondary" href="{{ route('subscribers.show', $subscriber->id) }}">
+                                        <a class="btn btn-outline-secondary"
+                                            href="{{ route('subscribers.show', $subscriber->id) }}">
                                             <i class="bi bi-eye-fill"></i>
                                         </a>
-                                        <a class="btn btn-outline-success" href="{{ route('subscribers.edit', $subscriber->id) }}">
+                                        <a class="btn btn-outline-success"
+                                            href="{{ route('subscribers.edit', $subscriber->id) }}">
                                             <i class="bi bi-vector-pen"></i>
                                         </a>
                                         <form action="{{ route('subscribers.destroy', $subscriber) }}" method="POST">
                                             @csrf
                                             @method('DELETE')
-                                            <button type="submit" onclick="return confirm('¿Desea eliminar el Suscriptor?')" class="btn btn-outline-danger"><i class="bi bi-trash-fill"></i></button>
+                                            <button type="submit"
+                                                onclick="return confirm('¿Desea eliminar el Suscriptor?')"
+                                                class="btn btn-outline-danger"><i class="bi bi-trash-fill"></i></button>
                                         </form>
                                     </div>
 
@@ -77,54 +83,68 @@
 @endsection
 
 @section('js')
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.bootstrap5.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js">
+    </script>
+    <script type="text/javascript" src="https://cdn.datatables.net/responsive/2.3.0/js/responsive.bootstrap5.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/select/1.4.0/js/dataTables.select.min.js"></script>
+
     <script>
         $(document).ready(function() {
             /* inicializando libreria requiere ID tabla */
             var t = $('#subscriptor-list').DataTable({
-                "order": [
+
+                dom: 'Bfrtip',
+                buttons: [
+                    {
+                    extend: 'excelHtml5',
+                    text: 'Excel',
+                    titleAttr: 'Exportar a Excel',
+                    title: 'Suscriptores Centro de Desarrollo Humano Colombia',
+                    exportOptions: {
+                        columns: [0, 1, 2, 3, 4]
+                    }
+                },
+                {
+                    extend: 'pdfHtml5',
+                    text: 'PDF',
+                    titleAttr: 'Exportar a PDF',
+                    title: 'Suscriptores Centro de Desarrollo Humano Colombia',
+                    exportOptions: {
+                        columns: [0, 1, 2, 3, 4]
+                    }
+                },
+                {
+                    extend: 'print',
+                    text: 'Imprimir',
+                    titleAttr: 'Imprimir',
+                    exportOptions: {
+                        columns: [0, 1, 2, 3, 4]
+                    }
+                }
+                ],
+                order: [
                     /* Ordenamiento predeterminado indice en base cero*/
                     [1, "desc"]
                 ],
-                "columnDefs": [{
+                columnDefs: [{
                     /* columna no busqueda indice en base cero*/
                     "targets": [5],
                     "searchable": false,
                 }],
-                "buttons": [{
-                        extend: 'excelHtml5',
-                        text: 'Exportar a Excel',
-                        titleAttr: 'Exportar a Excel',
-                        title: 'Mensajes Recibidos',
-                        exportOptions: {
-                            columns: [0, 1, 2, 3, 4, 5]
-                        }
-                    },
-                    {
-                        extend: 'pdfHtml5',
-                        text: 'Exportar a PDF',
-                        titleAttr: 'Exportar a PDF',
-                        className: 'btn btn-danger',
-                        title: 'Mensajes Recibidos',
-                        exportOptions: {
-                            columns: [0, 1, 2, 3, 4, 5]
-                        }
-                    },
-                    {
-                        extend: 'print',
-                        text: 'Imprimir',
-                        titleAttr: 'Imprimir',
-                        className: 'btn btn-info',
-                        exportOptions: {
-                            columns: [0, 1, 2, 3, 4, 5]
-                        }
-                    }
-                ],
-                "language": {
-                    "url": "https://cdn.datatables.net/plug-ins/1.10.21/i18n/Spanish.json"
+                language: {
+                "url": "https://cdn.datatables.net/plug-ins/1.10.21/i18n/Spanish.json"
                 }
+
 
             });
             /* Generar index de fila automaticamente */
